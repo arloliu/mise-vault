515454dde04b7c31517f60c91822c91e54533890
Arlo Liu <arlo.liu@gmail.com>
2026-08-20 21:40:10 +0800

    chore: bump plugin version to 0.8.0

--- metadata.lua
+++ metadata.lua
@@ -2,7 +2,7 @@
 --- from private Nexus, with the approval catalog bundled in this repo.
 PLUGIN = {
     name = "vault",
-    version = "0.7.0",
+    version = "0.8.0",
     description = "Company-approved developer tools from private Nexus",
     author = "devtools",
 }
